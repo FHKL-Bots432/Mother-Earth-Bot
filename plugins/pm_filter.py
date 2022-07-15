4c626854a871c927c0a1c865192f24a2facb492f
FilmyHangama ⇌ KannadaLegends <96911602+FHKL-Bots432@users.noreply.github.com>
2022-07-16 00:17:57 +0530

    ..............

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -501,7 +501,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-     elif query.data == "fs":
+     elif query.data == "filestore":
         buttons = [[
             InlineKeyboardButton('👩‍🦯 Back', callback_data='help')
         ]]
